48aa79c7356499ac88bd07337b991b1ad69e1d57
StevenM <stevenbmendoza@gmail.com>
2022-03-17 23:39:55 -0700

    generate page function modularized

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = (profileDataArr) => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
+// console.log(profileDataArgs);
 
-printProfileData(profileDataArgs);
+// const printProfileData = (profileDataArr) => {
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1]; will be turned into...
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html',generatePage(name,github), err =>{
+    if(err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
